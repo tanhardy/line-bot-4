98c011f647a142e7e166757911f9e1ab39a1c634
dekcomgigkok <32566085+dekcomgigkok@users.noreply.github.com>
2017-10-10 16:58:29 +0700

    Add files via upload

--- robots.php
+++ robots.php
@@ -185,6 +185,19 @@ if($arrJson['events'][0]['message']['type']=="sticker"){
     $arrPostData['messages'][0]['text'] = "พรีเมียร์ ขอเพิ่มอีก 3 ไม้น๊าาาาาา โฮ่งๆ โฮ่งๆ";
   }
 
+  $keys5_3 = array('อาบน้ำกัน','ไปอาบน้ำ');
+  $texts5_3 = $arrJson['events'][0]['message']['text'];
+  if(match($keys5_3, $texts5_3)){
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "image";
+    $arrPostData['messages'][0]['originalContentUrl'] = "https://whispering-dusk-19966.herokuapp.com/2.jpg";
+    $arrPostData['messages'][0]['previewImageUrl'] = "https://whispering-dusk-19966.herokuapp.com/2.jpg";
+
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = "พรีเมียร์ ขอไปเดินเล่นแปปน๊าาาา โฮ่งๆ โฮ่งๆ";
+  }
+
   $keys6 = array('นอนนะ', 'นอนได้', 'ไปนอน', 'นอนละ');
   $texts6 = $arrJson['events'][0]['message']['text'];
   if(match($keys6, $texts6)){
